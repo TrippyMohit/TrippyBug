8a61b3c1bfd4bbbf162181ef14b2e9b092968d4b
trippyMohit <mohitnegi.trippybug@gmail.com>
2023-01-07 14:17:13 +0530

    slug error one

--- src/pages/hotels.tsx
+++ src/pages/hotels.tsx
@@ -32,20 +32,7 @@ const HotelBanner = () => {
   return (
     <div className="flex flex-col">
       <div className="relative lg:min-h-[60vh] lg:mx-24 mx-8 lg:items-center items-start lg:rounded-3xl overflow-hidden lg:border-2 lg:flex-row flex-col  flex lg:shadow-lg  gap-10 lg:p-16">
-        <div className=" flex-1 flex flex-col gap-9 z-10 ">
-          {/* <div className="flex flex-col  lg:w-2/3 w-full gap-6">
-            <h1 className="text-3xl tracking-wider lg:text-6xl lg:leading-[70px] lg:text-gray-900 font-bold">
-              Spend your vacation with
-              <span className="lg:text-gray-900 text-orange-400 lg:font-sans font-salsa">
-                TrippyBug
-              </span>
-            </h1>
-            <p className="text-2xl flex lg:text-gray-900 text-gray-400 ">
-              Enter a country, a city or even just a landmark and we&apos;ll
-              find the right tours for you
-            </p>
-          </div> */}
-        </div>
+        <div className=" flex-1 flex flex-col gap-9 z-10 "></div>
         <div className="py-64 z-0 lg:flex hidden">
           <Image
             alt=""
