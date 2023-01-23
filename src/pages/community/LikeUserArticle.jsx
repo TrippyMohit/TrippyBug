7a318b19325ec36189f812d57e087ec4d43351ca
trippyMohit <mohitnegi.trippybug@gmail.com>
2023-01-23 15:15:56 +0530

    unliked featured added

--- src/pages/community/LikeUserArticle.jsx
+++ src/pages/community/LikeUserArticle.jsx
@@ -1,72 +1,10 @@
-// import React, { useState, useEffect } from "react";
-// import { useAuthState } from "react-firebase-hooks/auth";
-// import { auth, db } from "../../../firebase";
-// import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
-// import { AiFillLike, AiOutlineConsoleSql } from "react-icons/ai";
-
-// export default function LikeUserArticle({ articleId, likes }) {
-//   const [user] = useAuthState(auth);
-//   const [userUid, setUserUid] = useState();
-//   useEffect(() => {
-//     setUserUid(user.uid);
-//   });
-//   const likesRef = doc(db, "Articles", articleId);
-//   console.log(userUid);
-//   // updating data in already existing data in firebase DB
-
-//   const handleLike = () => {
-//     if (likes?.includes(userUid)) {
-//       updateDoc(likesRef, {
-//         likes: arrayRemove(userUid),
-//       })
-//         .then(() => {
-//           console.log("unliked");
-//         })
-//         .catch((e) => {
-//           console.log(e);
-//         });
-//     } else {
-//       updateDoc(likesRef, {
-//         likes: arrayUnion(userUid),
-//       })
-//         .then(() => {
-//           console.log(articleId);
-//           console.log("liked");
-//         })
-//         .catch((e) => {
-//           console.log(e);
-//         });
-//     }
-//   };
-//   return (
-//     <div>
-//       <button
-//         onClick={handleLike}
-//         className={`fa fa-heart${!likes?.includes(userUid) ? "-o" : ""} fa-lg`}
-//         style={{
-//           cursor: "pointer",
-//           color: likes?.includes(userUid) ? "red" : null,
-//         }}
-//       >
-//         <AiFillLike className="h-[25px] w-[25px] " />
-//       </button>
-//     </div>
-//   );
-// }
-
 import React, { useState, useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../../../firebase";
-import {
-  arrayRemove,
-  arrayUnion,
-  collection,
-  doc,
-  updateDoc,
-} from "firebase/firestore";
-
+import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
 import { AiFillLike, AiOutlineConsoleSql } from "react-icons/ai";
-const LikeUserArticle = ({ articleId }) => {
+
+export default function LikeUserArticle({ articleId, likes }) {
   const [user] = useAuthState(auth);
   const [likeRef, setLikeRef] = useState();
   useEffect(() => {
@@ -77,24 +15,43 @@ const LikeUserArticle = ({ articleId }) => {
     getLikeRef();
   }, [articleId]);
 
-  const handleClicked = () => {
-    updateDoc(likeRef, {
-      likes: arrayUnion(user.uid),
-    })
-      .then(() => {
-        console.log("liked");
-        console.log(likeRef);
+  // updating data in already existing data in firebase DB
+
+  const handleLike = () => {
+    if (likes?.includes(user.uid)) {
+      updateDoc(likeRef, {
+        likes: arrayRemove(user.uid),
       })
-      .catch((error) => {
-        console.log(error);
-      });
+        .then(() => {
+          console.log("unliked");
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    } else {
+      updateDoc(likeRef, {
+        likes: arrayUnion(user.uid),
+      })
+        .then(() => {
+          console.log("liked");
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
   };
-  console.log(likeRef);
   return (
     <div>
-      <button onClick={handleClicked}>Like</button>
+      <button
+        onClick={handleLike}
+        className={`fa fa-heart${!likes?.includes(user.uid) ? "-o" : ""} fa-lg`}
+        style={{
+          cursor: "pointer",
+          color: likes?.includes(user.uid) ? "red" : null,
+        }}
+      >
+        <AiFillLike className="h-[25px] w-[25px] " />
+      </button>
     </div>
   );
-};
-
-export default LikeUserArticle;
+}
