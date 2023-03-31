c09b20f3888f50912c329c26c05dfc5be13e25e1
trippyMohit <mohitnegi.trippybug@gmail.com>
2023-03-31 17:42:54 +0530

    sitemap alignment fixed

--- src/pages/sitemap.jsx
+++ src/pages/sitemap.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Sitemap = () => {
   return (
     <div
-      className="lg:ml-[90px]"
+      className="mr-[50px] py-16 lg:ml-[250px]"
       dangerouslySetInnerHTML={{
         __html: `
   <!DOCTYPE HTML>
