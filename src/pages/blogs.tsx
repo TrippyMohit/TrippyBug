116480f7d34cb44fa05f7fb8d1446bef652a5139
trippyMohit <mohitnegi.trippybug@gmail.com>
2023-04-03 17:46:57 +0530

    gallery cms blogs removed

--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -24,55 +24,78 @@ export default function Blogs({ blogPosts }) {
 }
 
 const PostCard = ({ post }) => {
+  const [hideBlog, setHideBlog] = useState(false);
+  const galleryTitles = [
+    "MainGallery",
+    "Wildlife",
+    "Skyscrapers",
+    "Nature",
+    "Nature",
+    "Mountains",
+    "Landscape",
+    "Island",
+    "Forest",
+    "Cities",
+    "Beach",
+  ];
+
+  useEffect(() => {
+    if (galleryTitles.includes(post?.node?.title)) {
+      setHideBlog(true);
+    }
+    console.log(hideBlog);
+  }, []);
+
+  const showBlog = hideBlog ? "hidden" : "block";
+
   return (
-    <Link href={`/${post?.node?.slug}`}>
-      <a className="w-full ">
-        <div className="flex cursor-pointer flex-col justify-between gap-4 bg-white shadow-lg rounded-2xl m-4 p-8 border border-gray-200 text-left">
-          <div className="flex justify-between w-full">
-            <div className="flex gap-5">
-              {post?.node?.author?.node?.avatar?.url && (
-                <div className="relative overflow-hidden  bg-white w-12 h-12 rounded-full items-center">
-                  <Image
-                    alt="avatar"
-                    src={post?.node?.author?.node?.avatar?.url}
-                    objectFit="cover"
-                    layout="fill"
-                  />
-                </div>
-              )}
-              <div className="flex flex-col justify-center ">
-                <div className="font-semibold text-lg text-gray-900  ">
-                  {post?.node?.author?.node?.name}
-                </div>
-                <div className="font-normal text-sm text-gray-400">
-                  {formatDistance(new Date(post?.node?.date), new Date(), {
-                    addSuffix: true,
-                  })}
+    <div className={`${showBlog}`}>
+      <Link href={`/${post?.node?.slug}`}>
+        <a className="w-full ">
+          <div className="flex cursor-pointer flex-col justify-between gap-4 bg-white shadow-lg rounded-2xl m-4 p-8 border border-gray-200 text-left">
+            <div className="flex justify-between w-full">
+              <div className="flex gap-5">
+                {post?.node?.author?.node?.avatar?.url && (
+                  <div className="relative overflow-hidden  bg-white w-12 h-12 rounded-full items-center">
+                    <Image
+                      alt="avatar"
+                      src={post?.node?.author?.node?.avatar?.url}
+                      objectFit="cover"
+                      layout="fill"
+                    />
+                  </div>
+                )}
+                <div className="flex flex-col justify-center ">
+                  <div className="font-semibold text-lg text-gray-900  ">
+                    {post?.node?.author?.node?.name}
+                  </div>
+                  <div className="font-normal text-sm text-gray-400">
+                    {formatDistance(new Date(post?.node?.date), new Date(), {
+                      addSuffix: true,
+                    })}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div className="flex flex-col gap-2 tracking-wider  flex-1 justify-between">
-            <h1 className="blogHeading font-semibold text-2xl text-gray-900 hover:text-orange-500 ">
-              {post?.node?.title}
-            </h1>
-
-            <div className="relative w-full flex-1 min-h-[200px]">
-              <Image
-                alt={post?.node?.title}
-                src={post?.node?.featuredImage?.node?.sourceUrl}
-                objectFit="cover"
-                layout="fill"
+            <div className="flex flex-col gap-2 tracking-wider  flex-1 justify-between">
+              <h1 className="blogHeading font-semibold text-2xl text-gray-900 hover:text-orange-500 ">
+                {post?.node?.title}
+              </h1>
+              <div className="relative w-full flex-1 min-h-[200px]">
+                <Image
+                  alt={post?.node?.title}
+                  src={post?.node?.featuredImage?.node?.sourceUrl}
+                  objectFit="cover"
+                  layout="fill"
+                />
+              </div>
+              <div
+                className="blogText font-normal text-base text-gray-500"
+                dangerouslySetInnerHTML={{ __html: post?.node?.excerpt }}
               />
-            </div>
-
-            <div
-              className="blogText font-normal text-base text-gray-500"
-              dangerouslySetInnerHTML={{ __html: post?.node?.excerpt }}
-            />
-            {/* like comment share icons */}
-            {/* <div className="flex gap-7 items-center pt-4">
+              {/* like comment share icons */}
+              {/* <div className="flex gap-7 items-center pt-4">
               <div className="flex gap-2 ">
                 <div className="w-6 h-6 text-gray-400">{LikeIcon}</div>
                 {0}
@@ -88,10 +111,11 @@ const PostCard = ({ post }) => {
                 <div>{0}</div>
               </div>
             </div> */}
+            </div>
           </div>
-        </div>
-      </a>
-    </Link>
+        </a>
+      </Link>
+    </div>
   );
 };
 
